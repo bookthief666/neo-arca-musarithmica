70950ff56bf41044c59a09af6763f422db73f03e
bookthief666 <manuel.orrantia@gmail.com>
2026-09-11 19:46:13 -0700

    feat(m1.1): define canonical Historica realm

---
+++ frontend/src/realms/historica.ts
@@ -0,0 +1,10 @@
+import type { ArcaRealmDefinition } from './types'
+
+export const HISTORICA_REALM = Object.freeze<ArcaRealmDefinition>({
+  id: 'historica',
+  label: 'Arca Historica',
+  ceremonialLabel: 'Reliquary Cabinet',
+  availability: 'available',
+  cssScope: 'historica',
+  guidanceVoice: 'scholarly',
+})
